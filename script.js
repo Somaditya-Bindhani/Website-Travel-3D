20f97dc5196c4284b876d7d28e7665b1db5f4275
Somaditya-Bindhani <84403111+Somaditya-Bindhani@users.noreply.github.com>
2021-05-26 02:45:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ let scene = new ScrollMagic.Scene({
 .setPin("section")
 .addTo(controller);
 
+
+(function type(){
+    if (count===texts.length){
+        count=0;
+    }
+    currentText=texts[count];
+    letter=currentText.slice(0,++index);
+    document.querySelector(".main-title").textContent=letter;
+    if(letter.length===currentText.length){
+        count++;
+        index=0;
+    }
+    setTimeout(type,300);
+
+})();
+
